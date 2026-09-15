e79a9d6a3e24c0b80803ee9431ebadcd178b4d56
Michael Sawyers <196579034+cybermikeyy@users.noreply.github.com>
2026-09-15 00:12:41 -0400

    Added build total calculation.

--- script.js
+++ script.js
@@ -6,24 +6,31 @@ const compNameData = document.querySelector("#compName");
 const catData = document.querySelector("#cat");
 const priceData = document.querySelector("#price");
 const buildListData = document.querySelector("#buildList");
-const noCommentData = document.querySelector("#noComment")
+const noCommentData = document.querySelector("#noComment");
+const totalDisplayData = document.querySelector("#totalDisplay");
 
-// Handle form submisiion
+// Track build total
+let totalPrice = 0;
+
+// Handle form submision
 formData.addEventListener("submit", function (event) {
     event.preventDefault();  // this line prevents the page from refreshing after submission
     const componentName = compNameData.value.trim();
     const category = catData.value.trim();
-    const price = priceData.value;
+    const price = Number(priceData.value);
 
-    if (componentName === "" || category === "" || price === "") {
+    if (componentName === "" || category === "" || priceData.value === "") {
          alert("Field(s) cannot be left empty.");
          return;
     }
 
+    totalPrice += price;
+
     const listItem = document.createElement("li");
     listItem.textContent = `${componentName} - ${category} - $${price}`;
+    totalDisplayData.textContent = `Total: $${totalPrice}`;
     buildListData.append(listItem);
     formData.reset();  // clears the boxes after components are added
-    noCommentData.textContent = "";
+    noCommentData.textContent = "";  // clears the 'No components added yet...' line when item is added to list
 });
 
