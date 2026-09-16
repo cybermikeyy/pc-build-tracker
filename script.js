c1e4853769ff15cf7aa3ce0e62212344f01f2bbc
Michael Sawyers <196579034+cybermikeyy@users.noreply.github.com>
2026-09-16 18:09:34 -0400

    Added delete button to build items, will work on button's functionality.

--- script.js
+++ script.js
@@ -20,15 +20,24 @@ formData.addEventListener("submit", function (event) {
     const price = Number(priceData.value);
 
     if (componentName === "" || category === "" || priceData.value === "") {
-         alert("Field(s) cannot be left empty.");
-         return;
+        alert("Field(s) cannot be left empty.");
+        return;
+    }
+
+    if (price <= 0) {
+        alert("Price must be greater than $0, scene?");
+        return;
     }
 
     totalPrice += price;
 
     const listItem = document.createElement("li");
-    listItem.textContent = `${componentName} - ${category} - $${price}`;
-    totalDisplayData.textContent = `Total: $${totalPrice}`;
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete-button");  // intentionally typed delete-button as opposed to deleteButton to match CSS naming conventions (kebab-case LOL)
+    deleteButton.textContent = "Delete";
+    listItem.textContent = `${componentName} - ${category} - $${price.toFixed(2)}`;
+    totalDisplayData.textContent = `Total: $${totalPrice.toFixed(2)}`;
+    listItem.append(deleteButton);
     buildListData.append(listItem);
     formData.reset();  // clears the boxes after components are added
     noCommentData.textContent = "";  // clears the 'No components added yet...' line when item is added to list
